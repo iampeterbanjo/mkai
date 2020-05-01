9f01c67c1a186d9592c49747d47322260b33cbd3
iampeterbanjo <gitblame@iampeterbanjo.com>
2020-05-02 00:29:26 +0100

    part 3

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,15 +1,23 @@
+import ScoreLabel from "../ui/ScoreLabel";
+import Phaser from "phaser";
+
 const GROUND_KEY = "ground";
 const DUDE_KEY = "dude";
+const STAR_KEY = "star";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super("game-scene");
+
     this.player = undefined;
+    this.cursors = undefined;
+    this.scoreLabel = undefined;
   }
 
   preload() {
     this.load.image("sky", "assets/sky.png");
     this.load.image(GROUND_KEY, "assets/platform.png");
+    this.load.image(STAR_KEY, "assets/star.png");
     this.load.image("star", "assets/star.png");
     this.load.image("bomb", "assets/bomb.png");
 
@@ -25,8 +33,11 @@ export default class GameScene extends Phaser.Scene {
 
     const platforms = this.createPlatforms();
     this.player = this.createPlayer();
+    const stars = this.createStars();
 
+    this.scoreLabel = this.createScoreLabel(16, 16, 0);
     this.physics.add.collider(this.player, platforms);
+    this.physics.add.overlap(this.player, stars, this.collectStar, null, this);
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
@@ -50,6 +61,25 @@ export default class GameScene extends Phaser.Scene {
     }
   }
 
+  createStars() {
+    const stars = this.physics.add.group({
+      key: STAR_KEY,
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    });
+
+    // stars.children.iterate((child) => {
+    //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    // });
+
+    return stars;
+  }
+
+  collectStar(player, star) {
+    star.disableBody(true, true);
+    this.scoreLabel.add(10);
+  }
+
   createPlatforms() {
     const platforms = this.physics.add.staticGroup();
 
@@ -60,6 +90,15 @@ export default class GameScene extends Phaser.Scene {
     platforms.create(750, 220, GROUND_KEY);
 
     return platforms;
+  }
+
+  createScoreLabel(x, y, score) {
+    const style = { fontSize: "32px", fill: "#000" };
+    const label = new ScoreLabel(this, x, y, score, style);
+
+    this.add.existing(label);
+
+    return label;
   }
 
   createPlayer() {
