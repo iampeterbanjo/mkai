42f36896c2a7af845dc78f2cc252bb8d695786a3
iampeterbanjo <gitblame@iampeterbanjo.com>
2020-05-13 21:59:06 +0100

    go back to working state

--- src/sounds.ts
+++ src/sounds.ts
@@ -3,234 +3,151 @@ import Pizzicato from 'pizzicato';
 const { scribble } = window;
 
 export const piano = {
-  BUTTON_3: () => {
-    const clip = scribble.clip({
+  BUTTON_3: scribble.clip({
     sample: './assets/sounds/piano48.wav',
     pattern: '[x]',
   }),
-  BUTT
-  return { start: clip.start, stop: clip.stop }ON_4: ()
-  =>} {
-    const clip = scribble.clip({
+  BUTTON_4: scribble.clip({
     sample: './assets/sounds/piano49.wav',
     pattern: '[x]',
   }),
-  BUTT
-  return { start: clip.start, stop: clip.stop }ON_1: ()
-  =>} {
-    const clip = scribble.clip({
+  BUTTON_1: scribble.clip({
     sample: './assets/sounds/piano50.wav',
     pattern: '[x]',
   }),
-  BUTT
-  return { start: clip.start, stop: clip.stop }ON_2: ()
-  =>} {
-    const clip = scribble.clip({
+  BUTTON_2: scribble.clip({
     sample: './assets/sounds/piano51.wav',
     pattern: '[x]',
   }),
-  THRO
-  return { start: clip.start, stop: clip.stop }W: () =>
-  sc}ribble.clip({
+  THROW: scribble.clip({
     sample: './assets/sounds/piano52.wav',
     pattern: '[x]',
   }),
-  AMP: () => s}cribble.clip({
+  AMP: scribble.clip({
     sample: './assets/sounds/piano53.wav',
     pattern: '[x]',
   }),
-  LEFT_BOTTO}M_SHOULDER: () => {
-    const clip = scribble.clip({
+  LEFT_BOTTOM_SHOULDER: scribble.clip({
     sample: './assets/sounds/piano54.wav',
     pattern: '[x]',
   }),
-  RIGH
-  return { start: clip.start, stop: clip.stop }T_BOTTOM_
-  SHOULDER: () =}> {
-    const clip = scribble.clip({
+  RIGHT_BOTTOM_SHOULDER: scribble.clip({
     sample: './assets/sounds/piano55.wav',
     pattern: '[x]',
   }),
-  SELE
-  return { start: clip.start, stop: clip.stop }CT_BACK:
-  () => scribble.}clip({
+  SELECT_BACK: scribble.clip({
     sample: './assets/sounds/piano56.wav',
     pattern: '[x]',
   }),
-  START_FORWARD: () }=> {
-    const clip = scribble.clip({
+  START_FORWARD: scribble.clip({
     sample: './assets/sounds/piano57.wav',
     pattern: '[x]',
   }),
-  LEFT_S
-  return { start: clip.start, stop: clip.stop }TICK: (
-
-  ) => sc}ribble.clip({
+  LEFT_STICK: scribble.clip({
     sample: './assets/sounds/piano58.wav',
     pattern: '[x]',
   }),
-  RIGHT_STICK: () =}> {
-    const clip = scribble.clip({
+  RIGHT_STICK: scribble.clip({
     sample: './assets/sounds/piano59.wav',
     pattern: '[x]',
   }),
-  DPAD
-  return { start: clip.start, stop: clip.stop }_UP: () =
-  > scr}ibble.clip({
+  DPAD_UP: scribble.clip({
     sample: './assets/sounds/piano60.wav',
     pattern: '[x]',
   }),
-  DPAD_DOWN: () }=> {
-    const clip = scribble.clip({
+  DPAD_DOWN: scribble.clip({
     sample: './assets/sounds/piano61.wav',
     pattern: '[x]',
   }),
-  DPAD
-  return { start: clip.start, stop: clip.stop }_LEFT: ()
-   =>} {
-    const clip = scribble.clip({
+  DPAD_LEFT: scribble.clip({
     sample: './assets/sounds/piano62.wav',
     pattern: '[x]',
   }),
-  DPAD_R
-  return { start: clip.start, stop: clip.stop }IGHT: (
-
-  ) =}> {
-    const clip = scribble.clip({
+  DPAD_RIGHT: scribble.clip({
     sample: './assets/sounds/piano63.wav',
     pattern: '[x]',
   }),
-}
 };
-return { start: clip.start, stop: clip.stop }
 
 export const midi = {
-  BUTTON_3: () => {
-    const clip = scribble.clip({
+  BUTTON_3: scribble.clip({
     synth: 'PluckSynth',
     notes: 'a4',
     pattern: '[[x]]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  BUTTON_4: () => {
-    const clip = scribble.clip({
+  }),
+  BUTTON_4: scribble.clip({
     synth: 'PluckSynth',
     notes: 'a3',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  BUTTON_1: () => {
-    const clip = scribble.clip({
+  }),
+  BUTTON_1: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c0',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  BUTTON_2: () => {
-    const clip = scribble.clip({
+  }),
+  BUTTON_2: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c1',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  THROW: () => {
-    const clip = scribble.clip({
+  }),
+  THROW: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c2',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  AMP: () => {
-    const clip = scribble.clip({
+  }),
+  AMP: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c3',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  LEFT_BOTTOM_SHOULDER: () => {
-    const clip = scribble.clip({
+  }),
+  LEFT_BOTTOM_SHOULDER: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c4',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  RIGHT_BOTTOM_SHOULDER: () => {
-    const clip = scribble.clip({
+  }),
+  RIGHT_BOTTOM_SHOULDER: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c5',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  SELECT_BACK: () => {
-    const clip = scribble.clip({
+  }),
+  SELECT_BACK: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c6',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  START_FORWARD: () => {
-    const clip = scribble.clip({
+  }),
+  START_FORWARD: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c7',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  LEFT_STICK: () => {
-    const clip = scribble.clip({
+  }),
+  LEFT_STICK: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c8',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  RIGHT_STICK: () => {
-    const clip = scribble.clip({
+  }),
+  RIGHT_STICK: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c9',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  DPAD_UP: () => {
-    const clip = scribble.clip({
+  }),
+  DPAD_UP: scribble.clip({
     synth: 'PluckSynth',
     notes: 'c4',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  DPAD_DOWN: () => {
-    const clip = scribble.clip({
+  }),
+  DPAD_DOWN: scribble.clip({
     synth: 'PluckSynth',
     notes: 'd4',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  DPAD_LEFT: () => {
-    const clip = scribble.clip({
+  }),
+  DPAD_LEFT: scribble.clip({
     synth: 'PluckSynth',
     notes: 'e4',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
-  DPAD_RIGHT: () => {
-    const clip = scribble.clip({
+  }),
+  DPAD_RIGHT: scribble.clip({
     synth: 'PluckSynth',
     notes: 'f4',
     pattern: '[x]',
-  })
-  return { start: clip.start, stop: clip.stop }
-},
+  }),
 };
