fa8350adcd988e4448de1cb6064e9a837f82d729
iampeterbanjo <gitblame@iampeterbanjo.com>
2020-05-13 20:47:15 +0100

    clean-up and refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Gamepad } from './gamepad';
+import { sounds } from './sounds';
 
-const { scribble, Tone } = window;
+const { Tone } = window;
 
 let gamepad_is_connected = false;
 const getControlName = (names, index, extraPrefix) => {
@@ -9,112 +9,29 @@ const getControlName = (names, index, extraPrefix) => {
     : extraPrefix + (index - names.length + 1);
 };
 
-interface HandleButtonPressCallbackParams {
-  name: string;
-  value: number;
-  button: GamepadButton;
-}
-
-type HandleButtonPressCallback = (
-  params: HandleButtonPressCallbackParams
-) => void;
-
-const sounds = {
-  FACE_1: scribble.clip({
-    sample: '/assets/sounds/piano48.wav', // new property: sample
-    pattern: 'x',
-  }),
-  FACE_2: scribble.clip({
-    sample: '/assets/sounds/piano49.wav', // new property: sample
-    pattern: 'x',
-  }),
-  FACE_3: scribble.clip({
-    sample: '/assets/sounds/piano50.wav', // new property: sample
-    pattern: 'x',
-  }),
-  FACE_4: scribble.clip({
-    sample: '/assets/sounds/piano51.wav', // new property: sample
-    pattern: 'x',
-  }),
-  LEFT_TOP_SHOULDER: scribble.clip({
-    sample: '/assets/sounds/piano52.wav', // new property: sample
-    pattern: 'x',
-  }),
-  RIGHT_TOP_SHOULDER: scribble.clip({
-    sample: '/assets/sounds/piano53.wav', // new property: sample
-    pattern: 'x',
-  }),
-  LEFT_BOTTOM_SHOULDER: scribble.clip({
-    sample: '/assets/sounds/piano54.wav', // new property: sample
-    pattern: 'x',
-  }),
-  RIGHT_BOTTOM_SHOULDER: scribble.clip({
-    sample: '/assets/sounds/piano55.wav', // new property: sample
-    pattern: 'x',
-  }),
-  SELECT_BACK: scribble.clip({
-    sample: '/assets/sounds/piano56.wav', // new property: sample
-    pattern: 'x',
-  }),
-  START_FORWARD: scribble.clip({
-    sample: '/assets/sounds/piano57.wav', // new property: sample
-    pattern: 'x',
-  }),
-  LEFT_STICK: scribble.clip({
-    sample: '/assets/sounds/piano58.wav', // new property: sample
-    pattern: 'x',
-  }),
-  RIGHT_STICK: scribble.clip({
-    sample: '/assets/sounds/piano59.wav', // new property: sample
-    pattern: 'x',
-  }),
-  DPAD_UP: scribble.clip({
-    sample: '/assets/sounds/piano60.wav', // new property: sample
-    pattern: 'x',
-  }),
-  DPAD_DOWN: scribble.clip({
-    sample: '/assets/sounds/piano61.wav', // new property: sample
-    pattern: 'x',
-  }),
-  DPAD_LEFT: scribble.clip({
-    sample: '/assets/sounds/piano62.wav', // new property: sample
-    pattern: 'x',
-  }),
-  DPAD_RIGHT: scribble.clip({
-    sample: '/assets/sounds/piano63.wav', // new property: sample
-    pattern: 'x',
-  }),
-};
-
-const formatFloat = (n: number, places: number) => {
-  var m = Math.pow(10, places);
-  return '' + parseFloat('' + Math.round(n * m) / m).toFixed(places);
-};
-
 const active = {};
 
-const getAxisName = (sum: number): string => {
-  switch (sum) {
-    case 8:
-      return 'DPAD_DOWN';
-    case 6:
-      return 'DPAD_UP';
-    case 5:
-      return 'DPAD_LEFT';
-    case 7:
-      return 'DPAD_RIGHT';
-    default:
-      return 'uknown';
-  }
-};
+const STANDARD_BUTTONS = [
+  'FACE_1',
+  'FACE_2',
+  'FACE_3',
+  'FACE_4',
+  'LEFT_TOP_SHOULDER',
+  'RIGHT_TOP_SHOULDER',
+  'LEFT_BOTTOM_SHOULDER',
+  'RIGHT_BOTTOM_SHOULDER',
+  'SELECT_BACK',
+  'START_FORWARD',
+  'LEFT_STICK',
+  'RIGHT_STICK',
+  'DPAD_UP',
+  'DPAD_DOWN',
+  'DPAD_LEFT',
+  'DPAD_RIGHT',
+  'HOME',
+];
 
-const handleButtonPress = ({
-  up,
-  down,
-}: {
-  up: HandleButtonPressCallback;
-  down: HandleButtonPressCallback;
-}) => {
+const handleButtonPress = () => {
   for (const pad of navigator.getGamepads()) {
     if (pad) {
       pad.axes.forEach((axis, index) => {
@@ -151,21 +68,15 @@ const handleButtonPress = ({
         }
       });
       pad.buttons.forEach((button, index) => {
-        const name = getControlName(
-          Gamepad.StandardButtons,
-          index,
-          'EXTRA_BUTTON_'
-        );
-        if (button.value === 1) console.log(name);
+        const name = getControlName(STANDARD_BUTTONS, index, 'EXTRA_BUTTON_');
         if (sounds[name]) {
-          const params = { button, name, value: button.value };
           if (button.value === 1) {
             active[name] = true;
-            down(params);
+            sounds[name].start();
           }
           if (active[name] && button.value !== 1) {
             active[name] = false;
-            up(params);
+            sounds[name].stop();
           }
         }
       });
@@ -179,34 +90,13 @@ const runAnimation = () => {
   }
 
   window.requestAnimationFrame(runAnimation);
-  handleButtonPress({
-    up: ({ name, value }) => {
-      sounds[name].stop();
-    },
-    down: ({ name, value }) => {
-      sounds[name].start();
-    },
-  });
+  handleButtonPress();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log(`Let's GO!`);
 
   Tone.Transport.start();
-
-  const playButton = document.querySelector('.btn-play');
-  if (playButton) {
-    playButton.addEventListener('click', () => {
-      scribble
-        .clip({
-          synth: 'PolySynth', // new property: synth
-          pattern: '[xx]',
-          notes: 'C4 D4 C4 D#4 C4 D4 C4 Bb3',
-        })
-        .start();
-      console.log('click');
-    });
-  }
 
   window.addEventListener('gamepadconnected', (event) => {
     console.log('Gamepad connected');
---
+++ src/sounds.ts
@@ -0,0 +1,68 @@
+const { scribble, Tone } = window;
+
+export const sounds = {
+  FACE_1: scribble.clip({
+    sample: '/assets/sounds/piano48.wav',
+    pattern: 'x',
+  }),
+  FACE_2: scribble.clip({
+    sample: '/assets/sounds/piano49.wav',
+    pattern: 'x',
+  }),
+  FACE_3: scribble.clip({
+    sample: '/assets/sounds/piano50.wav',
+    pattern: 'x',
+  }),
+  FACE_4: scribble.clip({
+    sample: '/assets/sounds/piano51.wav',
+    pattern: 'x',
+  }),
+  LEFT_TOP_SHOULDER: scribble.clip({
+    sample: '/assets/sounds/piano52.wav',
+    pattern: 'x',
+  }),
+  RIGHT_TOP_SHOULDER: scribble.clip({
+    sample: '/assets/sounds/piano53.wav',
+    pattern: 'x',
+  }),
+  LEFT_BOTTOM_SHOULDER: scribble.clip({
+    sample: '/assets/sounds/piano54.wav',
+    pattern: 'x',
+  }),
+  RIGHT_BOTTOM_SHOULDER: scribble.clip({
+    sample: '/assets/sounds/piano55.wav',
+    pattern: 'x',
+  }),
+  SELECT_BACK: scribble.clip({
+    sample: '/assets/sounds/piano56.wav',
+    pattern: 'x',
+  }),
+  START_FORWARD: scribble.clip({
+    sample: '/assets/sounds/piano57.wav',
+    pattern: 'x',
+  }),
+  LEFT_STICK: scribble.clip({
+    sample: '/assets/sounds/piano58.wav',
+    pattern: 'x',
+  }),
+  RIGHT_STICK: scribble.clip({
+    sample: '/assets/sounds/piano59.wav',
+    pattern: 'x',
+  }),
+  DPAD_UP: scribble.clip({
+    sample: '/assets/sounds/piano60.wav',
+    pattern: 'x',
+  }),
+  DPAD_DOWN: scribble.clip({
+    sample: '/assets/sounds/piano61.wav',
+    pattern: 'x',
+  }),
+  DPAD_LEFT: scribble.clip({
+    sample: '/assets/sounds/piano62.wav',
+    pattern: 'x',
+  }),
+  DPAD_RIGHT: scribble.clip({
+    sample: '/assets/sounds/piano63.wav',
+    pattern: 'x',
+  }),
+};
