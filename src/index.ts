4da3f28e7b5db12b206b66ed3586da16342128fe
Peter Banjo <me@iampeterbanjo.com>
2020-05-16 14:32:46 +0000

    react to correct input string

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { generateSound, getVolume } from './sounds';
+import { generateSound, getVolume } from "./sounds";
 
 let gamepad_is_connected = false;
 const getControlName = (names, index, extraPrefix) => {
@@ -11,26 +11,40 @@ const active = {};
 const bank = {};
 
 const STANDARD_BUTTONS = [
-  'BUTTON_3',
-  'BUTTON_4',
-  'BUTTON_1',
-  'BUTTON_2',
-  'THROW',
-  'AMP',
-  'LEFT_BOTTOM_SHOULDER',
-  'RIGHT_BOTTOM_SHOULDER',
-  'SELECT_BACK',
-  'START_FORWARD',
-  'LEFT_STICK',
-  'RIGHT_STICK',
-  'DPAD_UP',
-  'DPAD_DOWN',
-  'DPAD_LEFT',
-  'DPAD_RIGHT',
-  'HOME',
+  "BUTTON_3",
+  "BUTTON_4",
+  "BUTTON_1",
+  "BUTTON_2",
+  "THROW",
+  "AMP",
+  "LEFT_BOTTOM_SHOULDER",
+  "RIGHT_BOTTOM_SHOULDER",
+  "SELECT_BACK",
+  "START_FORWARD",
+  "LEFT_STICK",
+  "RIGHT_STICK",
+  "DPAD_UP",
+  "DPAD_DOWN",
+  "DPAD_LEFT",
+  "DPAD_RIGHT",
+  "HOME",
 ];
 
 let frame = 0;
+let inputBuffer = [] as string[];
+const checkInputs = (name: string) => {
+  inputBuffer.push(name);
+  let buffer = inputBuffer.join(",");
+  console.log(buffer);
+  if (buffer === "DPAD_DOWN,DPAD_LEFT,BUTTON_1") {
+    console.log("BOOM");
+    inputBuffer = [];
+    playSound("BOOM", 50);
+  }
+  if (name === "SELECT_BACK" || inputBuffer.length === 3) {
+    inputBuffer = [];
+  }
+};
 
 const playSound = (name: string, index: number, type?: string) => {
   if (frame % 15 === 0) {
@@ -46,6 +60,9 @@ const stopSound = (name: string) => {
   if (!sound) {
     return;
   }
+  //   if (frame % 4 === 0) {
+  checkInputs(name);
+  //   }
   bank[name].stop();
 };
 
@@ -58,17 +75,15 @@ const handleAxis = (up: string, down: string) => (
     stopSound(down);
   }
   if (axis === -1) {
-    console.log(up);
-    playSound(up, value, 'sine');
+    playSound(up, value, "sine");
   }
   if (axis === 1) {
-    console.log(down);
-    playSound(down, value, 'sine');
+    playSound(down, value, "sine");
   }
 };
 
-const handleY = handleAxis('DPAD_UP', 'DPAD_DOWN');
-const handleX = handleAxis('DPAD_LEFT', 'DPAD_RIGHT');
+const handleY = handleAxis("DPAD_UP", "DPAD_DOWN");
+const handleX = handleAxis("DPAD_LEFT", "DPAD_RIGHT");
 
 const handleButtonPress = () => {
   for (const pad of navigator.getGamepads()) {
@@ -86,10 +101,9 @@ const handleButtonPress = () => {
         }
       });
       pad.buttons.forEach((button, index) => {
-        const name = getControlName(STANDARD_BUTTONS, index, 'EXTRA_BUTTON_');
+        const name = getControlName(STANDARD_BUTTONS, index, "EXTRA_BUTTON_");
 
         if (button.value === 1) {
-          console.log(name, index);
           active[name] = true;
           playSound(name, index);
         }
@@ -107,23 +121,23 @@ const runAnimation = () => {
     return;
   }
 
-  frame = frame === 60 ? 0 : frame + 1; 
+  frame = frame === 60 ? 0 : frame + 1;
   window.requestAnimationFrame(runAnimation);
   handleButtonPress();
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   console.log(`Let's GO!`);
 
-  window.addEventListener('gamepadconnected', (event) => {
-    console.log('Gamepad connected');
+  window.addEventListener("gamepadconnected", (event) => {
+    console.log("Gamepad connected");
     gamepad_is_connected = true;
 
     window.requestAnimationFrame(runAnimation);
   });
 
-  window.addEventListener('gamepaddisconnected', (event) => {
-    console.log('Gamepad disconnected:');
+  window.addEventListener("gamepaddisconnected", (event) => {
+    console.log("Gamepad disconnected:");
 
     gamepad_is_connected = false;
   });
